a6dc62360c8463732ea22b75b919f875a42459e4
Yavuz Selim Gügen <ygugen@windowslive.com>
2024-05-26 14:39:35 +0300

    feat: add product code generation

---
+++ Acme.Core/Interfaces/IProductCodeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Core.Interfaces
+{
+    public interface IProductCodeService
+    {
+        string GenerateUniqueCode();
+        bool ValidateCode(string code);
+    }
+}
--- Acme.Services/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Acme.Services
-{
-    public class Class1
-    {
-
-    }
-}
