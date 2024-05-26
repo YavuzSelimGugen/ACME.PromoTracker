01fc10abe8df8af6123fc85b523b4f35f2437f20
Yavuz Selim Gügen <ygugen@windowslive.com>
2024-05-26 16:30:23 +0300

    feat: add DI extension

---
+++ Acme.API/Extensions/BuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Acme.Core.Interfaces;
+using Acme.Repository;
+using Acme.Services;
+
+namespace Acme.API.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            services.AddTransient<IOcrService, OcrTestService>();
+            services.AddTransient<IProductCodeService, ProductCodeService>();
+            services.AddTransient<IProductCodeRepository, ProductCodeRepository>();
+            services.AddTransient<IReceiptProcessor, ReceiptProcessor>();
+            
+            return services;
+        }
+    }
+}
